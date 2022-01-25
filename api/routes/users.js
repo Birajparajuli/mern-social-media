92e89cef72a09812baff02bebc7ecbb6d07e07b4
Birajparajuli <birajparajuli2001@gmail.com>
2022-01-25 21:05:13 +0545

    user updated

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
-const user = require("../models/User");
+const User = require("../models/User");
 
 // update user
 router.put("/:id", async (req, res) => {
@@ -17,6 +17,7 @@ router.put("/:id", async (req, res) => {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
+
       res.status(200).json({ message: "Acount has been updated." });
     } catch (err) {
       res.status(500).json({ message: err });
