0d6a4f57957d8259fc43ab1bd89eaca97a8403cc
Birajparajuli <birajparajuli2001@gmail.com>
2022-02-02 13:29:36 +0545

    react router added

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Home />
-      {/* <Profile /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="profile/:username" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
