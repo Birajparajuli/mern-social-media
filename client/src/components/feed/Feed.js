af8373071074e42d8d569acd27f6a76aed108cb1
Birajparajuli <birajparajuli2001@gmail.com>
2022-02-03 17:02:41 +0545

    fetching data from api

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -2,7 +2,20 @@ import Post from "../post/Post";
 import Share from "../share/Share";
 import "./Feed.css";
 import { Posts } from "../../dummyData";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = axios.get("/timeline/61f0338be300543ff151b920");
+      console.log(res);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feed-wrapper">
